f41b804d62a8e6f338a334c3962ff60e15b14385
Chhaya Kakad <115397852+Chhayak3@users.noreply.github.com>
2025-05-21 09:51:12 +0530

    Update countscript.js

--- countscript.js
+++ countscript.js
@@ -2,7 +2,7 @@ const minusBtn=document.querySelector('.minus-btn')
 const plusBtn=document.querySelector('.plus-btn')
 const count=document.querySelector('.count')
 const changeBy=document.querySelector('.changeBy')
-
+const reset=document.querySelector('.reset-btn')
 plusBtn.addEventListener('click',() =>{
   const countValue=parseInt(count.innerText)
   const changeByValue=parseInt(changeBy.value)
@@ -15,4 +15,7 @@ minusBtn.addEventListener('click',() =>
   const changeByValue=parseInt(changeBy.value)
       count.innerText=countValue -changeByValue
     ;});
+ reset.addEventListener('click',() =>{
+      count.innerText=1;
+    })
    
