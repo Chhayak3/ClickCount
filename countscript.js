ec9ad6d82cae347e34b8d2184837055096816537
Chhaya Kakad <115397852+Chhayak3@users.noreply.github.com>
2025-05-21 09:18:24 +0530

    Add files via upload

---
+++ countscript.js
@@ -0,0 +1,18 @@
+const minusBtn=document.querySelector('.minus-btn')
+const plusBtn=document.querySelector('.plus-btn')
+const count=document.querySelector('.count')
+const changeBy=document.querySelector('.changeBy')
+
+plusBtn.addEventListener('click',() =>{
+  const countValue=parseInt(count.innerText)
+  const changeByValue=parseInt(changeBy.value)
+  count.innerText=countValue+changeByValue
+})
+
+minusBtn.addEventListener('click',() =>
+  {
+  const countValue=parseInt(count.innerText)
+  const changeByValue=parseInt(changeBy.value)
+      count.innerText=countValue -changeByValue
+    ;});
+   
